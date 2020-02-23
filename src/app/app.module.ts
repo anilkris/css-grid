10ecd6fd11b4f37f9857ff26d1a783baa546df0d
anil <anilkrishna@gmail.com>
2020-02-23 16:29:11 +0530

    updated grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MygridComponent } from './mygrid/mygrid.component';
+import { Grid2Component } from './grid2/grid2.component';
+import { Grid3Component } from './grid3/grid3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MygridComponent,
+    Grid2Component,
+    Grid3Component
   ],
   imports: [
     BrowserModule,
